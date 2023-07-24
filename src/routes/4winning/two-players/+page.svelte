d8da331de969379d4048f887505440a47b4547e0
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-07-24 23:20:50 +0200

    Updated 4winning

--- src/routes/4winning/two-players/+page.svelte
+++ src/routes/4winning/two-players/+page.svelte
@@ -90,8 +90,8 @@
       cell.style.backgroundColor = color;
       const Id = `${outerIndex + 1}-${innerIndex}`;
       currentTeam.data.push(Id);
-      console.log(teams);
     }
+    checkWin();
     changeTeam();
   }
 
@@ -100,6 +100,99 @@
     if (teamTurnDisplay) {
       teamTurnDisplay.innerHTML = `Current Team Turn: ${color}`;
     }
+  }
+
+  function generateWinCombinations(): {
+    cells: { outerIndex: number; innerIndex: number }[];
+  }[] {
+    const combinations: {
+      cells: { outerIndex: number; innerIndex: number }[];
+    }[] = [];
+
+    // Rows
+    for (let i = 0; i <= 55; i += 7) {
+      for (let j = i; j < i + 4; j++) {
+        combinations.push({
+          cells: [
+            { outerIndex: Math.floor(j / 7), innerIndex: j % 7 },
+            { outerIndex: Math.floor((j + 1) / 7), innerIndex: (j + 1) % 7 },
+            { outerIndex: Math.floor((j + 2) / 7), innerIndex: (j + 2) % 7 },
+            { outerIndex: Math.floor((j + 3) / 7), innerIndex: (j + 3) % 7 },
+          ],
+        });
+      }
+    }
+
+    // Columns
+    for (let i = 0; i < 7; i++) {
+      for (let j = i; j <= i + 21; j += 7) {
+        combinations.push({
+          cells: [
+            { outerIndex: Math.floor(j / 7), innerIndex: j % 7 },
+            { outerIndex: Math.floor((j + 7) / 7), innerIndex: (j + 7) % 7 },
+            { outerIndex: Math.floor((j + 14) / 7), innerIndex: (j + 14) % 7 },
+            { outerIndex: Math.floor((j + 21) / 7), innerIndex: (j + 21) % 7 },
+          ],
+        });
+      }
+    }
+
+    // Diagonal (top-left to bottom-right)
+    for (let i = 0; i <= 21; i += 7) {
+      for (let j = i; j <= i + 2; j++) {
+        combinations.push({
+          cells: [
+            { outerIndex: Math.floor(j / 7), innerIndex: j % 7 },
+            { outerIndex: Math.floor((j + 8) / 7), innerIndex: (j + 8) % 7 },
+            { outerIndex: Math.floor((j + 16) / 7), innerIndex: (j + 16) % 7 },
+            { outerIndex: Math.floor((j + 24) / 7), innerIndex: (j + 24) % 7 },
+          ],
+        });
+      }
+    }
+
+    // Diagonal (top-right to bottom-left)
+    for (let i = 3; i <= 23; i += 7) {
+      for (let j = i; j <= i + 2; j++) {
+        combinations.push({
+          cells: [
+            { outerIndex: Math.floor(j / 7), innerIndex: j % 7 },
+            { outerIndex: Math.floor((j + 6) / 7), innerIndex: (j + 6) % 7 },
+            { outerIndex: Math.floor((j + 12) / 7), innerIndex: (j + 12) % 7 },
+            { outerIndex: Math.floor((j + 18) / 7), innerIndex: (j + 18) % 7 },
+          ],
+        });
+      }
+    }
+
+    return combinations;
+  }
+
+  const winCombinations = generateWinCombinations();
+
+  function checkWin() {
+    for (const combination of winCombinations) {
+      const { cells } = combination;
+      const teamData = currentTeam.data;
+      let isWinningCombination = true;
+
+      for (const cell of cells) {
+        const { outerIndex, innerIndex } = cell;
+        const cellId = `${outerIndex}-${innerIndex}`;
+
+        if (!teamData.includes(cellId)) {
+          isWinningCombination = false;
+          break;
+        }
+      }
+
+      if (isWinningCombination) {
+        alert(`Team ${currentTeam.color} wins!`);
+        return true;
+      }
+    }
+
+    return false;
   }
 </script>
 
