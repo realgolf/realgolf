9e4f47c1b7df520e02233c5328a16d332280510d
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-07-27 14:26:18 +0200

    Updated 4winning and exact

--- src/routes/4winning/+page.svelte
+++ src/routes/4winning/+page.svelte
@@ -1,30 +1,31 @@
 <svelte:head>
-    <title>4 Winning</title>
-  </svelte:head>
-  
-  <h1>4 Winning</h1>
-  
+  <title>4 Winning</title>
+</svelte:head>
+
+<h1>4 Winning</h1>
+
+<main>
   <div>
     <h2>Rules</h2>
-  
+
     <p>
       The rules of the game are very simple, it's just a kind of four-in-a-row,
       you click the button which one you hit from the distance. After the first
       team's turn, the second teams is and clicks on a field, regardless of
       whether it has already been clicked on by another team, then it's the turn
-      of the next team, you can see which team it is at the top of the respective
-      game recognize. The game continues until one team has four squares in a row
-      or column. If you click the same field four times, a message appears telling
-      you that you have clicked the same field four times. When a team wins,
-      the values ​of the fields are reset and you can start from the beginning.
-      The game automatically switches teams. All of the above information applies
-      to all games!
+      of the next team, you can see which team it is at the top of the
+      respective game recognize. The game continues until one team has four
+      squares in a row or column. If you click the same field four times, a
+      message appears telling you that you have clicked the same field four
+      times. When a team wins, the values ​of the fields are reset and you can
+      start from the beginning. The game automatically switches teams. All of
+      the above information applies to all games!
     </p>
   </div>
-  
+
   <div>
     <h2>Levels</h2>
-  
+
     <ol>
       <li>Bronze: Only the lenght of the shot applies</li>
       <li>Silver: The number of meters and the lateral deviation apply.</li>
@@ -32,7 +33,7 @@
       <li>Platinum: Like gold, all with a club.</li>
     </ol>
   </div>
-  
+
   <div>
     <h2>Game modes</h2>
     <ol>
@@ -41,16 +42,16 @@
       <li><a href="/4winning/four-players">Four players / teams</a></li>
     </ol>
   </div>
-  
+
   <div>
     <h2>Further information:</h2>
-  
+
     <p>
-      This game is for golfers who want to train a different way, you only need a
-      kind of trackman or a simulator to determine the exact length. After you
-      have determined the length with your trackman (or the like), you can do that
-      with this Click length labeled field in the respective game. The games
-      automatically change the teams or the players through.
+      This game is for golfers who want to train a different way, you only need
+      a kind of trackman or a simulator to determine the exact length. After you
+      have determined the length with your trackman (or the like), you can do
+      that with this Click length labeled field in the respective game. The
+      games automatically change the teams or the players through.
       <br />
       <b>Have fun playing</b>
       <br />
@@ -59,8 +60,12 @@
       >
     </p>
   </div>
-  
-  <style lang="scss">
+</main>
+
+<style lang="scss">
+  main {
+    max-width: 40rem;
+    padding-inline: 1rem;
     div {
       display: block;
       p,
@@ -72,5 +77,5 @@
         list-style: upper-latin;
       }
     }
-  </style>
-  
+  }
+</style>
--- src/routes/exact/+page.svelte
+++ src/routes/exact/+page.svelte
@@ -4,53 +4,59 @@
 
 <h1>Exact</h1>
 
-<div>
-  <h2>Rules</h2>
+<main>
+  <div>
+    <h2>Rules</h2>
 
-  <ul>
-    <li>It is played alternately</li>
-    <li>Click the Field you hit.</li>
-    <li>Respect the levels</li>
-    <li>Only the intger number applies</li>
-  </ul>
-</div>
+    <ul>
+      <li>It is played alternately</li>
+      <li>Click the Field you hit.</li>
+      <li>Respect the levels</li>
+      <li>Only the intger number applies</li>
+    </ul>
+  </div>
 
-<div>
-  <h2>Levels</h2>
+  <div>
+    <h2>Levels</h2>
 
-  <ol>
-    <li>Bronze: Only the lenght of the shot applies</li>
-    <li>
-      Silver: The number of meters and the lateral deviation apply. - lateral
-      deviation gets choosed by the players
-    </li>
-    <li>
-      Gold: Fields can only be played after prior announcement. The rest is like
-      Silver
-    </li>
-    <li>Platinum: Like gold, all with a club.</li>
-  </ol>
-</div>
+    <ol>
+      <li>Bronze: Only the lenght of the shot applies</li>
+      <li>
+        Silver: The number of meters and the lateral deviation apply. - lateral
+        deviation gets choosed by the players
+      </li>
+      <li>
+        Gold: Fields can only be played after prior announcement. The rest is
+        like Silver
+      </li>
+      <li>Platinum: Like gold, all with a club.</li>
+    </ol>
+  </div>
 
-<div>
-  <h2>Game modes</h2>
-  <ol>
-    <li><a href="/exact/two-players">Two players / teams</a></li>
-    <li><a href="/exact/three-players">Three players / teams</a></li>
-    <li><a href="/exact/four-players">Four players / teams</a></li>
-  </ol>
-</div>
+  <div>
+    <h2>Game modes</h2>
+    <ol>
+      <li><a href="/exact/two-players">Two players / teams</a></li>
+      <li><a href="/exact/three-players">Three players / teams</a></li>
+      <li><a href="/exact/four-players">Four players / teams</a></li>
+    </ol>
+  </div>
+</main>
 
 <style lang="scss">
-  div {
-    display: block;
+  main {
+    max-width: 40rem;
+    padding-inline: 1rem;
+    div {
+      display: block;
 
-    ol {
-      margin-left: 4vw;
-      line-break: auto;
-    }
-    ol {
-      list-style: upper-latin;
+      ol {
+        margin-left: 4vw;
+        line-break: auto;
+      }
+      ol {
+        list-style: upper-latin;
+      }
     }
   }
 </style>
