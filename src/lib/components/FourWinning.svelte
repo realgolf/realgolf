154256d4404b18b859c189f1953f8bd572275b52
MoinJulian <jh@moinjulian.com>
2023-08-16 12:44:54 +0200

    Finished Input distance for 4Winning

--- src/lib/components/FourWinning.svelte
+++ src/lib/components/FourWinning.svelte
@@ -289,6 +289,12 @@
    */
 
   function restartGame() {
+    let input = document.getElementById("distance") as HTMLInputElement;
+
+    if (input) {
+      input.value = "";
+    }
+
     localStorage.removeItem(`4winning_team_${teams.length}`);
 
     teams.forEach((team) => {
@@ -310,6 +316,12 @@
   }
 
   function restartGame_Btn() {
+    let input = document.getElementById("distance") as HTMLInputElement;
+
+    if (input) {
+      input.value = "";
+    }
+
     localStorage.removeItem(`4winning_team_${teams.length}`);
 
     teams.forEach((team) => {
@@ -347,65 +359,129 @@
           if (distance >= 10 && distance <= 23) {
             let rowIndex = Math.floor((distance - 10) / 2);
             let cell = document.getElementById(`row1-${rowIndex}`);
+            let Id = `1-${rowIndex}`;
 
             if (cell) {
               cell.style.backgroundColor = currentTeam.color;
+              currentTeam.data.push(Id);
+              localStorage.setItem(
+                `4winning_team_${teams.length}`,
+                JSON.stringify(teams)
+              );
+
+              checkWin();
               changeTeam();
             }
           } else if (distance >= 24 && distance <= 37) {
             let rowIndex = Math.floor((distance - 24) / 2);
             let cell = document.getElementById(`row2-${rowIndex}`);
+            let Id = `2-${rowIndex}`;
 
             if (cell) {
               cell.style.backgroundColor = currentTeam.color;
+              currentTeam.data.push(Id);
+              localStorage.setItem(
+                `4winning_team_${teams.length}`,
+                JSON.stringify(teams)
+              );
+
+              checkWin();
               changeTeam();
             }
           } else if (distance >= 38 && distance <= 58) {
             let rowIndex = Math.floor((distance - 38) / 3);
             let cell = document.getElementById(`row3-${rowIndex}`);
+            let Id = `3-${rowIndex}`;
 
             if (cell) {
               cell.style.backgroundColor = currentTeam.color;
+              currentTeam.data.push(Id);
+              localStorage.setItem(
+                `4winning_team_${teams.length}`,
+                JSON.stringify(teams)
+              );
+
+              checkWin();
               changeTeam();
             }
           } else if (distance >= 59 && distance <= 79) {
             let rowIndex = Math.floor((distance - 59) / 3);
             let cell = document.getElementById(`row4-${rowIndex}`);
+            let Id = `4-${rowIndex}`;
 
             if (cell) {
               cell.style.backgroundColor = currentTeam.color;
+              currentTeam.data.push(Id);
+              localStorage.setItem(
+                `4winning_team_${teams.length}`,
+                JSON.stringify(teams)
+              );
+
+              checkWin();
               changeTeam();
             }
           } else if (distance >= 80 && distance <= 100) {
             let rowIndex = Math.floor((distance - 80) / 3);
             let cell = document.getElementById(`row5-${rowIndex}`);
+            let Id = `5-${rowIndex}`;
 
             if (cell) {
               cell.style.backgroundColor = currentTeam.color;
+              currentTeam.data.push(Id);
+              localStorage.setItem(
+                `4winning_team_${teams.length}`,
+                JSON.stringify(teams)
+              );
+
+              checkWin();
               changeTeam();
             }
           } else if (distance >= 101 && distance <= 121) {
             let rowIndex = Math.floor((distance - 101) / 3);
             let cell = document.getElementById(`row6-${rowIndex}`);
+            let Id = `6-${rowIndex}`;
 
             if (cell) {
               cell.style.backgroundColor = currentTeam.color;
+              currentTeam.data.push(Id);
+              localStorage.setItem(
+                `4winning_team_${teams.length}`,
+                JSON.stringify(teams)
+              );
+
+              checkWin();
               changeTeam();
             }
           } else if (distance >= 122 && distance <= 142) {
             let rowIndex = Math.floor((distance - 122) / 3);
             let cell = document.getElementById(`row7-${rowIndex}`);
+            let Id = `7-${rowIndex}`;
 
             if (cell) {
               cell.style.backgroundColor = currentTeam.color;
+              currentTeam.data.push(Id);
+              localStorage.setItem(
+                `4winning_team_${teams.length}`,
+                JSON.stringify(teams)
+              );
+
+              checkWin();
               changeTeam();
             }
-          }else if (distance >= 143 && distance <= 163) {
+          } else if (distance >= 143 && distance <= 163) {
             let rowIndex = Math.floor((distance - 142) / 3);
             let cell = document.getElementById(`row8-${rowIndex}`);
+            let Id = `8-${rowIndex}`;
 
             if (cell) {
               cell.style.backgroundColor = currentTeam.color;
+              currentTeam.data.push(Id);
+              localStorage.setItem(
+                `4winning_team_${teams.length}`,
+                JSON.stringify(teams)
+              );
+
+              checkWin();
               changeTeam();
             }
           } else {
