90979dbb818ae9ef6dab525124ea490d97528ca0
MoinJulian <jh@moinjulian.com>
2023-08-16 16:35:21 +0200

    Get data from the localStorage

--- src/routes/dashboard/4winning/two-players/+page.svelte
+++ src/routes/dashboard/4winning/two-players/+page.svelte
@@ -10,6 +10,36 @@
     { color: "red", data: [] },
     { color: "blue", data: [] },
   ];
+
+  function saveToDataBase() {
+    let localStorageData = localStorage.getItem("4winning_team_2");
+
+    if (localStorageData) {
+      const parsedData = JSON.parse(localStorageData);
+
+      if (Array.isArray(parsedData)) {
+        const redTeam = parsedData.find((team) => team.color === "red");
+        const blueTeam = parsedData.find((team) => team.color === "blue");
+
+        if (redTeam) {
+          const existingRedTeam = teams.find((t) => t.color === "red");
+          if (existingRedTeam) {
+            existingRedTeam.data = redTeam.data;
+          }
+        }
+
+        if (blueTeam) {
+          const existingBlueTeam = teams.find((t) => t.color === "blue");
+          if (existingBlueTeam) {
+            existingBlueTeam.data = blueTeam.data;
+          }
+        }
+        console.log(redTeam, blueTeam);
+      }
+    }
+  }
 </script>
 
 <FourWinning {teams} />
+
+<button on:click={saveToDataBase}>Save in Database</button>
