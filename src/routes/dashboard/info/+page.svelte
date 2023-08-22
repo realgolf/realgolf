3a8254265224e7d22a906d9b0a3a378fefc3ba65
MoinJulian <jh@moinjulian.com>
2023-08-22 17:35:46 +0200

    Created file for info

---
+++ src/routes/dashboard/info/+page.svelte
@@ -0,0 +1,16 @@
+<h2>Information:</h2>
+
+<p>
+  The games are for golfers who want to train a different way, you only need a
+  kind of trackman or a simulator to determine the exact length. After you have
+  determined the length with your trackman (or the like). You can click the
+  Field with number you played or enter the number in the input Field.
+  <br />
+  <b>Have fun playing</b>
+</p>
+
+<p>
+  A list of some things that can be used can be found <a href="/dashboard/tools"
+    >here</a
+  >.
+</p>
