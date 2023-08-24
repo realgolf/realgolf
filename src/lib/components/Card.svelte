8d0aeca44de77ff2052d1816312867dfa6a1ad8e
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-08-24 23:56:12 +0200

    Added details to cards

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,16 +1,43 @@
 <script lang="ts">
   export let title: string;
   export let link: string;
+
+  let showNormal = true;
+  let showHover = false;
+  function toggleDetails() {
+    showNormal = !showNormal;
+    showHover = !showHover;
+  }
 </script>
 
-<a class="card" href={link}>
-  <div>
+<div class:card_normal={!showHover} class:hidden={showHover}>
+  <a href={link}>
     <h3>{title}</h3>
-  </div>
-</a>
+  </a>
+  <button on:click={toggleDetails}>Details</button>
+</div>
+
+<div class:card_hover={!showNormal} class:hidden={showNormal}>
+  <h4>{title}</h4>
+  {#if title == "Rules"}
+    <p>Here you can see all rules for all three games.</p>
+  {:else if title == "Modi"}
+    <p>
+      Here you can find all game modes you can play, just click on the link for
+      the game you want to play.
+    </p>
+  {:else if title == "Levels"}
+    <p>Some extra variation, if the games are to easy for you.</p>
+  {:else if title == "Games"}
+    <p>Here you can find all games you have saved in the database.</p>
+  {:else if title == "Info"}
+    <p>Just some random further information.</p>
+  {/if}
+  <button on:click={toggleDetails}>{title}</button>
+</div>
 
 <style lang="scss">
-  .card {
+  .card_normal {
     background-color: var(--nav-color);
     width: max-content;
     padding: 50px 50px;
@@ -24,5 +51,22 @@
     h3 {
       text-align: center;
     }
+  }
+  .card_hover {
+    background-color: var(--nav-color);
+    width: 20rem;
+    padding: 50px 50px;
+    border-radius: 5px;
+    border: 1px solid var(--border-color);
+    margin: 15px 30px;
+    text-decoration: none;
+  }
+
+  button {
+    margin-top: 10px;
+  }
+
+  .hidden {
+    display: none;
   }
 </style>
