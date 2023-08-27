e9e6f04d6b4369373ea521a437c15759e97ecd64
Julian Hammer <jh@moinjulian.com>
2023-08-28 00:16:09 +0200

    Clean up

--- site/server/static/darkmode.js
+++ site/server/static/darkmode.js
@@ -1,13 +1,3 @@
-// const saved_theme = localStorage.getItem('theme');
-// if (saved_theme) {
-// 	document.body.setAttribute('data-theme', saved_theme);
-// } else {
-// const prefers_dark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-// const theme = prefers_dark ? "dark" : "light";
-// document.body.setAttribute("data-theme", theme);
-// localStorage.setItem("theme", theme);
-// }
-
 function handlePrefersDarkChange(event) {
   const newTheme = event.matches ? "dark" : "light";
   document.body.setAttribute("data-theme", newTheme);
