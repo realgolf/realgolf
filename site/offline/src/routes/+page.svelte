28d695a77bccc5b357e7e6bbef8f13b6cd0110df
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-08-28 13:39:16 +0200

    Modified +page.svelte

--- site/offline/src/routes/+page.svelte
+++ site/offline/src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+	<title>Golf Games offline</title>
+</svelte:head>
+
+<h1>Golf Games offline</h1>
+
+<main>
+	<p>
+		<b>
+			Welcome to "Golf Games" - your ultimate destination for golf challenges! Discover "4 Winning",
+			"Exact", and "Precision" - captivating golf experiences tested with trainers.
+		</b>
+	</p>
+	<br />
+	<p>
+		This is the offline version of the origial version <a target="_blank" href="https://golf.moinjulian.com"
+			>Golf Games</a
+		>, which has a user authentication.
+	</p>
+</main>
