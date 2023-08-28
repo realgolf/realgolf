51de1c588b564c8c119d9af5b6240f8aa5d8f577
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-08-28 22:36:16 +0200

    doesn't need translation

---
+++ site/de/offline/src/routes/games/precision/two-players/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Precision from "$lib/components/Precision.svelte";
+
+  let point = 100;
+
+  interface Team {
+    color: string;
+    points: number;
+    distance: number;
+  }
+
+  let teams: Team[] = [
+    { color: "red", points: point, distance: 0 },
+    { color: "blue", points: point, distance: 0 },
+  ];
+</script>
+
+<Precision {teams} {point} />
