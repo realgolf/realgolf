d8394fc77e09b07eadba1d408919ab4440a4a849
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-08-28 21:59:36 +0200

    translated exact two teams server

---
+++ site/de/server/src/routes/dashboard/exact/two-players/+page.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { enhance } from "$app/forms";
+  import Exact from "$lib/components/Exact.svelte";
+  import type { ActionData } from "./$types";
+
+  let form: ActionData;
+
+  interface Team {
+    color: string;
+    data: number[];
+    points: number;
+  }
+
+  let teams: Team[] = [
+    { color: "red", data: [], points: 0 },
+    { color: "blue", data: [], points: 0 },
+  ];
+
+  function saveToDataBase() {
+    // Extract data from localStorage
+    const localStorageData = localStorage.getItem("exact_2_data");
+    console.log(localStorageData);
+
+    if (localStorageData) {
+      teams = JSON.parse(localStorageData);
+      console.log(teams);
+    }
+  }
+
+  function saveToLS() {
+    let gameData = document.getElementById("game") as HTMLInputElement;
+    console.log(gameData.value);
+
+    localStorage.setItem("exact_2_data", gameData.value);
+
+    location.reload();
+  }
+</script>
+
+<Exact {teams} />
+
+<p>
+  Fügen Sie die Daten ein, die Sie von <a href="/dashboard/games">Spiele</a> erhalten
+  haben:
+</p>
+<input type="text" id="game" name="game" />
+<button on:click={saveToLS}>Abschicken</button>
+<br />
+<button on:click={saveToDataBase}
+  >Extrahieren Sie Daten für die Datenbank.</button
+>
+
+<form method="POST" autocomplete="off" use:enhance>
+  <input
+    type="text"
+    name="team_data"
+    id="team_data"
+    value={JSON.stringify(teams)}
+  />
+  <button>In die Datenbank übertragen</button>
+</form>
+
+<style>
+  button {
+    margin-top: 2rem;
+  }
+</style>
