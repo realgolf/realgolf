97441be6c5281ccd656dc70cdcd0f7c3226cf283
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-08-28 15:11:20 +0200

    Edit games styles

--- site/offline/src/routes/games/4winning/+page.svelte
+++ site/offline/src/routes/games/4winning/+page.svelte
@@ -48,3 +48,19 @@
 		</main>
 	</div>
 </main>
+
+<style lang="scss">
+	main {
+		display: flex;
+		flex-direction: row;
+		justify-content: left;
+		align-items: left;
+		flex-wrap: wrap; /* Elemente unterhalb der Breite umbrechen */
+
+		@media screen and (max-width: 767px) {
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+		}
+	}
+</style>
