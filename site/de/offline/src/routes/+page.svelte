2f0a80276421929b5de3bd69d55ea64630954b04
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-08-28 22:31:58 +0200

    translated +page.svelte

---
+++ site/de/offline/src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<svelte:head>
+	<title>Golf Games offline</title>
+</svelte:head>
+
+<h1>Golf Games offline</h1>
+
+<main>
+	<p>
+		<b>
+			Willkommen zu "Golf Games" - Ihrer ultimative Adresse für Golf Challenges! Entdecken Sie "4
+			Winning", "Exact", und "Precision" - eine fesselnde Golf Erfahrung mit Trainern.
+		</b>
+	</p>
+	<br />
+	<p>
+		Dies ist die Offline-Version der ursprünglichen Version <a
+			target="_blank"
+			href="https://de.golf.moinjulian.com">Golf Games</a
+		>, die über eine Benutzerauthentifizierung verfügt.
+	</p>
+</main>
