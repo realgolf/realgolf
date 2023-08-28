4b953a847c70aaacbd41d0fff272dcbc33819d77
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-08-28 22:32:24 +0200

    doesn't need translation

---
+++ site/de/offline/src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Nav from '$lib/components/Nav.svelte';
+	import ThemeToggler from '$lib/components/ThemeToggler.svelte';
+	import '$lib/css/app.css';
+</script>
+
+<svelte:head>
+	<link rel="icon" href="/favicon16x16.png" />
+</svelte:head>
+
+<Nav />
+
+<main>
+	<slot />
+</main>
+
+<style>
+	main {
+		max-width: 40rem;
+		/* margin-inline: auto; */
+		padding-inline: 1rem;
+	}
+</style>
