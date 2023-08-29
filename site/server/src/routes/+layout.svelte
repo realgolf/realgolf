616c325ff4bf8bd17af6ff7c1bb501c0df4def81
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-08-29 12:11:19 +0200

    Added LoadProgress and ScrollUp

--- site/server/src/routes/+layout.svelte
+++ site/server/src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+  import LoadProgress from "$lib/components/LoadProgress.svelte";
   import Nav from "$lib/components/Nav.svelte";
+  import ScrollUp from "$lib/components/ScrollUp.svelte";
   import "$lib/css/app.css";
   import type { LayoutData } from "./$types";
   export let data: LayoutData;
@@ -8,6 +10,9 @@
 <svelte:head>
   <link rel="icon" href="/favicon16x16.png" />
 </svelte:head>
+
+<LoadProgress />
+<ScrollUp />
 
 <Nav logged_in={!!data.name && !!data.email} />
 
