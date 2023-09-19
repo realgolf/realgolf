131e9627ee9d424ef48c84311a5f6a4e8d074508
MoinJulian <jh@moinjulian.com>
2023-09-19 21:17:46 +0100

    Add target blank

--- site/server/src/routes/+page.svelte
+++ site/server/src/routes/+page.svelte
@@ -35,12 +35,14 @@
     You can play the offline version of the game <a
       target="_blank"
       href="https://golf-offline.moinjulian.com">here</a
-    >. You will need a network connection for the initial load, but after that, all
-    the data will be cached, allowing you to play without an internet connection.
+    >. You will need a network connection for the initial load, but after that,
+    all the data will be cached, allowing you to play without an internet
+    connection.
   </p>
 
   <p>
     If you have any questions, please visit our <a
+      target="_blank"
       href="https://golf-faq.moinjulian.com">FAQ</a
     >
     section first to see if your question has already been answered. If you cannot
