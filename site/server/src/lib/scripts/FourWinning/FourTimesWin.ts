b1f1fc34f9f622bc654abb9effb4b463ff7e332f
MoinJulian <jh@moinjulian.com>
2023-09-20 15:07:43 +0100

    Export 4 Winning FourTimesWin

---
+++ site/server/src/lib/scripts/FourWinning/FourTimesWin.ts
@@ -0,0 +1,28 @@
+import { restartGame } from "./restartGame";
+
+interface Team {
+  color: string;
+  data: string[];
+}
+
+export function FourTimesWin(
+  cellId: string,
+  teamColor: string,
+  teams: Team[],
+  hitCounts: {},
+  currentTeam: Team,
+  currentTeamIndex: number,
+  color: string
+) {
+  const confirmed = confirm(
+    `Cell ${cellId} has been hit four times by ${teamColor} team!`
+  );
+
+  if (confirmed) {
+    const confirmed2 = confirm(`Do you want to restart the game?`);
+
+    if (confirmed2) {
+      restartGame(teams, hitCounts, currentTeam, currentTeamIndex, color);
+    }
+  }
+}
