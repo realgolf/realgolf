0c3692ebb7e475d8f83866c0135479e441ed0d28
MoinJulian <jh@moinjulian.com>
2023-09-27 10:49:51 +0100

    Add links

--- site/offline/src/lib/components/Dropdownmenu.svelte
+++ site/offline/src/lib/components/Dropdownmenu.svelte
@@ -13,16 +13,26 @@
 	<button class="dropdown-button" on:click={toggleDropdown}>
 		<Fa icon={faBars} />
 	</button>
-	{#if isOpen}
-		<div class="dropdown-content">
-			<a href="/">Home</a>
-			<br />
-			<a href="/games/4winning">4 Winning</a>
-		</div>
-	{/if}
 </div>
+{#if isOpen}
+	<div class="dropdown-content">
+		<a href="/">Home</a>
+		<div class="games row">
+			<a href="/games/4winning">4 Winning</a>
+			<a href="/games/exact">Exact</a>
+			<a href="/games/precision">Precision</a>
+		</div>
+		<div class="all row">
+			<a href="/imprint">Imprint</a>
+		</div>
+		<div class="external-links row">
+			<a href="https://golf.moinjulian.com">Server</a>
+			<a href="https://golf-faq.moinjulian.com">FAQ</a>
+		</div>
+	</div>
+{/if}
 
-<style>
+<style lang="scss">
 	/* Basic styling for the dropdown */
 	.dropdown {
 		position: relative;
@@ -54,5 +64,23 @@
 		display: block;
 		right: 0;
 		left: auto;
+		padding-right: 30px;
+		height: max-content;
+		text-align: left;
+
+		.games {
+			border-top: 2px solid var(--border-color);
+			padding-bottom: 3px;
+			border-bottom: 2px solid var(--border-color);
+		}
+
+		.external-links {
+			padding-bottom: 3px;
+		}
+
+		.row {
+			display: flex;
+			flex-direction: column;
+		}
 	}
 </style>
