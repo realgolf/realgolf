647006c36f9a0b10e06ba3885c9387e8cf19793d
Julian Hammer <100045739+MoinJulian@users.noreply.github.com>
2023-10-01 18:17:04 +0100

    Delete card details

--- site/server/src/lib/components/Card.svelte
+++ site/server/src/lib/components/Card.svelte
@@ -4,6 +4,7 @@
 
   let showNormal = true;
   let showHover = false;
+
   function toggleDetails() {
     showNormal = !showNormal;
     showHover = !showHover;
@@ -14,28 +15,6 @@
   <a href={link}>
     <h3>{title}</h3>
   </a>
-  <button on:click={toggleDetails}>Details</button>
-</div>
-
-<div class:card_hover={!showNormal} class:hidden={showNormal}>
-  <a href={link}>
-    <h3>{title}</h3>
-  </a>
-  {#if title == "Rules"}
-    <p>Here you can see all rules for all three games.</p>
-  {:else if title == "Modi"}
-    <p>
-      Here you can find all game modes you can play, just click on the link for
-      the game you want to play.
-    </p>
-  {:else if title == "Levels"}
-    <p>Some extra variation, if the games are to easy for you.</p>
-  {:else if title == "Games"}
-    <p>Here you can find all games you have saved in the database.</p>
-  {:else if title == "Info"}
-    <p>Just some random further information.</p>
-  {/if}
-  <button on:click={toggleDetails}>{title}</button>
 </div>
 
 <style lang="scss">
@@ -69,44 +48,5 @@
     .card_normal {
       width: 80vw;
     }
-  }
-  .card_hover {
-    background-color: var(--nav-color);
-    padding: 50px 50px;
-    border-radius: 5px;
-    border: 1px solid var(--border-color);
-    margin: 15px;
-    text-align: left;
-    &:hover {
-      cursor: pointer;
-      box-shadow: 10px 10px 20px var(--border-color);
-    }
-    h3 {
-      text-align: center;
-    }
-  }
-
-  /* Auf Desktop: Karten nehmen die gesamte Breite ein */
-  @media (min-width: 768px) {
-    .card_hover {
-      width: calc(
-        40vw
-      ); /* Abzug von 30px Marge, um sicherzustellen, dass die Karten die gesamte Breite einnehmen */
-    }
-  }
-
-  /* Auf Handy: Karten sind 90vw breit */
-  @media (max-width: 767px) {
-    .card_hover {
-      width: 80vw;
-    }
-  }
-
-  button {
-    margin-top: 10px;
-  }
-
-  .hidden {
-    display: none;
   }
 </style>
