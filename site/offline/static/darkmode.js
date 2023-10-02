d903c56a37517e11ced37af62a611b3089b2ebf5
Julian Hammer <100045739+MoinJulian@users.noreply.github.com>
2023-10-02 16:22:08 +0100

    Updated ThemeToggler

--- site/offline/static/darkmode.js
+++ site/offline/static/darkmode.js
@@ -1,17 +1,9 @@
-function handlePrefersDarkChange(event) {
-  const newTheme = event.matches ? "dark" : "light";
-  document.body.setAttribute("data-theme", newTheme);
+const saved_theme = localStorage.getItem('theme');
+if (saved_theme) {
+	document.body.setAttribute('data-theme', saved_theme);
+} else {
+	const prefers_dark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+	const theme = prefers_dark ? 'dark' : 'light';
+	document.body.setAttribute('data-theme', theme);
+	localStorage.setItem('theme', theme);
 }
-
-function setupPrefersDarkListener() {
-  const prefersDarkMediaQuery = window.matchMedia(
-    "(prefers-color-scheme: dark)"
-  );
-  handlePrefersDarkChange(prefersDarkMediaQuery);
-
-  // Listen for changes in the prefers-color-scheme media query
-  prefersDarkMediaQuery.addEventListener("change", handlePrefersDarkChange);
-}
-
-// Initial setup
-setupPrefersDarkListener();
