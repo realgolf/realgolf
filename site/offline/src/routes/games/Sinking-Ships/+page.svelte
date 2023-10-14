b6fe5b2e0cd8c21ccad4fcf5cf579afff0fa2b2c
Julian Hammer <100045739+MoinJulian@users.noreply.github.com>
2023-10-14 17:33:03 +0200

    Translated Rules to English

--- site/offline/src/routes/games/Sinking-Ships/+page.svelte
+++ site/offline/src/routes/games/Sinking-Ships/+page.svelte
@@ -9,32 +9,36 @@
 		<h2>Rules</h2>
 
 		<div>
+			<h3>Board:</h3>
 			<p>
-				Spielfeld: Jeder Spieler hat ein Spielfeld, das in Spalten von A bis J und in Zeilen von 1
-				bis 10 unterteilt ist. Dies ergibt ein Raster von 10x10 Feldern. Schiffe: Jeder Spieler hat
-				eine Flotte von Schiffen, die auf seinem Spielfeld platziert werden. Die üblichen Schiffe
-				und ihre Größen sind:
+				Each Player has a board, that is splitted into columns from A to J and in rows from 1 to 10.
+				This makes a grid of 10x10 fields.
 			</p>
-
+			<h3>Ships:</h3>
+			<p>Each Player has a fleet of ships, which are placed on his board.</p>
+			<h4>The Ships and sizes are:</h4>
 			<div class="margin-left">
-				<li>1 Schlachtschiff (5 Felder)</li>
-				<li>1 Kreuzer (4 Felder)</li>
-				<li>2 Zerstörer (jeweils 3 Felder)</li>
-				<li>2 U-Boote (jeweils 2 Felder)</li>
-				<li>2 Patrouillenboote (jeweils 1 Feld)</li>
+				<li>1 Battleship (5 fields)</li>
+				<li>1 Cruiser (4 fields)</li>
+				<li>2 Destroyers (3 fields each)</li>
+				<li>2 Submarines (2 fields each)</li>
+				<li>2 patrol boats (1 field each)</li>
 			</div>
+			<h3>Placement of the Ships:</h3>
 			<p>
-				Platzierung der Schiffe: Die Spieler platzieren abwechselnd ihre Schiffe auf ihrem
-				Spielfeld, ohne dass sich die Schiffe berühren oder überlappen dürfen. Die genaue Position
-				der Schiffe bleibt geheim. Spielverlauf: Die Spieler machen abwechselnd Schüsse, um die
-				Schiffe ihres Gegners zu finden. Ein Schuss wird durch die Koordinaten angegeben, z. B.
-				"A3". Der Spieler, der am Zug ist, sagt "Treffer" oder "Versenkt", wenn sein Schuss auf ein
-				feindliches Schiff trifft oder dieses versenkt. Der getroffene Spieler markiert das
-				Spielfeld des Gegners, um getroffene Schiffe anzuzeigen. Gewinnen: Das Spiel endet, wenn ein
-				Spieler alle Schiffe seines Gegners versenkt hat. Dieser Spieler gewinnt das Spiel.
-				Sonderregel: Manchmal wird vereinbart, dass ein Spieler nach einem Treffer ein weiteres
-				Schussrecht hat, um zu versuchen, die Länge des getroffenen Schiffs zu ermitteln und es
-				schneller zu versenken.
+				The first Player places his Ships first on the Board, while the other Players look away,
+				then the second and so on until all Players have placed all their Ships.
+			</p>
+			<h3>Playing style with more than two Players:</h3>
+			<p>
+				If more than two Players play the game, each Player can decide who he wants to attack, for
+				example if there are the Players "Red", "Blue" and "Green", then "Red" can decide, if he
+				wants to attack "Blue" or "Green" first, the Player with spare Ships is winning at the end.
+			</p>
+			<h3>Special regulation:</h3>
+			<p>
+				You can agree that a player has another shot after a hit to try to determine the length of
+				the hit ship and sink it faster.
 			</p>
 		</div>
 	</div>
