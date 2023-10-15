7fd253342a496d6766f8b385dd4edb88a8d555b3
Julian Hammer <100045739+MoinJulian@users.noreply.github.com>
2023-10-15 20:54:37 +0200

    Add Footer and License

---
+++ docs/src/lib/components/Footer.svelte
@@ -0,0 +1,28 @@
+<footer>
+	<div class="content">
+		<p>This is the Documentation for the Golf Games</p>
+		<p><a href="https://golf.moinjulian.com">The Games</a></p>
+		<p>
+			This documentation are Open-Source, so you are very welcome to make a contribution towards
+			improving this documentation
+		</p>
+		<a href="https://github.com/moinjulian/golf">
+			<button>Make a contribution</button>
+		</a>
+	</div>
+</footer>
+
+<style lang="scss">
+	footer {
+		width: 100vw;
+		background-color: var(--nav-color);
+		margin-top: 2rem;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		.content {
+			padding: 3rem 4rem;
+		}
+	}
+</style>
