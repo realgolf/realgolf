9e50cfd637702ac45952995b6646043b9a74d39c
Julian Hammer <100045739+MoinJulian@users.noreply.github.com>
2023-10-18 16:19:33 -0400

    Add ToC

--- docs/src/routes/[id]/+page.svelte
+++ docs/src/routes/[id]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	export let data;
 
+	$: toc = data.toc;
 	$: html_code = data.html_code;
 	$: title = data.attributes.title;
 </script>
@@ -13,11 +14,90 @@
 	{title}
 </h1>
 
+{#if toc.length > 0}
+	<details class="toc">
+		<summary>Table of Contents</summary>
+		<ol>
+			{#each toc as item}
+				<li>
+					<a href="#{item.id}">{item.text}</a>
+				</li>
+			{/each}
+		</ol>
+	</details>
+
+	<div class="outer-toc" class:long={toc.length > 15} aria-hidden="true">
+		<ol>
+			{#each toc as item}
+				<li>
+					<a tabindex="-1" href="#{item.id}">{item.text}</a>
+				</li>
+			{/each}
+		</ol>
+	</div>
+{/if}
+
 <article>
 	{@html html_code}
 </article>
 
 <style lang="scss">
+	.toc {
+		margin-top: 1rem;
+		margin-bottom: 1rem;
+
+		ol {
+			margin-top: 0.5rem;
+		}
+	}
+
+	.outer-toc {
+		opacity: 0;
+		pointer-events: none;
+
+		@media (min-width: 68rem) {
+			opacity: 1;
+			pointer-events: initial;
+		}
+
+		transition: opacity 250ms linear;
+		position: fixed;
+		top: 0;
+		height: 100vh;
+		transform: translateX(78rem);
+		overflow-y: auto;
+
+		display: flex;
+		align-items: center;
+		color: var(--secondary-font-color);
+
+		ol {
+			margin-left: 0;
+			padding-left: 2rem;
+			border-left: 1px solid var(--border-color);
+		}
+
+		a:hover {
+			color: var(--font-color);
+		}
+
+		&.long {
+			align-items: start;
+
+			li + li {
+				margin-block: 0;
+			}
+
+			ol {
+				padding-block: 1rem;
+				height: 100vh;
+				display: flex;
+				flex-direction: column;
+				justify-content: space-between;
+			}
+		}
+	}
+
 	article {
 		:global(h2) {
 			padding-top: 5rem;
