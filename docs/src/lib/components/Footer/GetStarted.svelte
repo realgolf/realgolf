7808c0f1064b565fad1328b0230c1b98fb41f9b4
Julian Hammer <100045739+MoinJulian@users.noreply.github.com>
2023-10-18 22:41:43 +0200

    Updated Get Started

--- docs/src/lib/components/Footer/GetStarted.svelte
+++ docs/src/lib/components/Footer/GetStarted.svelte
@@ -1,6 +1,6 @@
 <div class="column">
 	<h3>Get started</h3>
-	<a href="/create-an-account">Create an Account</a>
+	<a href="/about-accounts">About Accounts</a>
 	<a href="/preparation-for-the-games">Preparation for the Games</a>
 </div>
 
