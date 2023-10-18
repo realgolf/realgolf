ee7e849cee08c04a98d66e30c724aa3f31ec9255
Julian Hammer <100045739+MoinJulian@users.noreply.github.com>
2023-10-18 18:48:05 -0400

    Updated Nav

--- docs/src/lib/components/de/Nav.svelte
+++ docs/src/lib/components/de/Nav.svelte
@@ -11,7 +11,7 @@
 
 	let links: link[] = [
 		{
-			path: '/',
+			path: '/de',
 			icon: faHouse
 		}
 	];
--- docs/src/lib/components/en/Nav.svelte
+++ docs/src/lib/components/en/Nav.svelte
@@ -11,7 +11,7 @@
 
 	let links: link[] = [
 		{
-			path: '/',
+			path: '/en',
 			icon: faHouse
 		}
 	];
