0533d63cc715793fc195c41677b4d26dc94d5369
Julian Hammer <100045739+MoinJulian@users.noreply.github.com>
2023-10-24 23:19:47 -0400

    removed themeToggler form Dropdown Menu

--- site/server/src/lib/components/Dropdownmenu.svelte
+++ site/server/src/lib/components/Dropdownmenu.svelte
@@ -18,7 +18,6 @@
   } from "@fortawesome/free-solid-svg-icons";
   import { onDestroy, onMount } from "svelte";
   import Fa from "svelte-fa";
-  import ThemeToggler from "./ThemeToggler.svelte";
 
   export let auth: string | undefined;
 
@@ -110,9 +109,6 @@
           href="https://github.com/MoinJulian/Golf">GitHub</a
         >
       </p>
-    </div>
-    <div class="row theme-toggler">
-      <p><ThemeToggler /></p>
     </div>
   </div>
 {/if}
@@ -218,8 +214,7 @@
     .copyright,
     .games,
     .logged-out,
-    .logged-in,
-    .theme-toggler {
+    .logged-in {
       padding-top: 10px;
       padding-bottom: 10px;
     }
