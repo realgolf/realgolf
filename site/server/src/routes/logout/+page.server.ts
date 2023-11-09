ce136b73be02d1d22ab1f811f17509e8a463ca1d
Julian Hammer <100045739+MoinJulian@users.noreply.github.com>
2023-11-09 19:40:12 +0000

    Add load function on /logout

--- site/server/src/routes/logout/+page.server.ts
+++ site/server/src/routes/logout/+page.server.ts
@@ -1,4 +1,12 @@
 import { redirect, type Actions } from "@sveltejs/kit";
+import type { PageServerLoad } from "./$types";
+
+export const load: PageServerLoad = async (event) => {
+  event.cookies.delete("auth-token");
+  event.cookies.delete("email");
+  event.cookies.delete("name");
+  throw redirect(301, "/");
+};
 
 export const actions: Actions = {
   default: async (event) => {
