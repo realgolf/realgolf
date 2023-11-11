353477ba5b8c54e2d2123a208470f2e604125ec1
Julian Hammer <100045739+MoinJulian@users.noreply.github.com>
2023-11-11 15:09:51 +0000

    Generated table

---
+++ site/server/src/routes/dashboard/modi/Sinking-Ships/Two-Players/+page.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import { first_row, rows } from "./table";
+</script>
+
+<svelte:head>
+  <title>Sinking Ships- 2 Teas</title>
+</svelte:head>
+
+<h1>2 Teas</h1>
+
+<table>
+  {#each first_row as { side, data }, i (i)}
+    <tr>
+      <td class="points">{side}</td>
+      {#each data as item, j (j)}
+        <td class="points">{item}</td>
+      {/each}
+    </tr>
+  {/each}
+  {#each rows as { side, data }, i (i)}
+    <tr>
+      <td class="points">{side}</td>
+      {#each data as item, j (j)}
+        <td class="meters">{item}</td>
+      {/each}
+    </tr>
+  {/each}
+</table>
+
+<style>
+  .meters,
+  .points {
+    flex: 0 0 25%;
+    box-sizing: border-box;
+    border: 5px solid var(--border-color);
+    min-width: calc(90vw / 15);
+    height: calc(50vh / 8);
+    text-align: center;
+  }
+
+  .meters:hover {
+    cursor: pointer;
+  }
+
+  button {
+    cursor: pointer;
+    border: 5px solid var(--border-color);
+  }
+</style>
