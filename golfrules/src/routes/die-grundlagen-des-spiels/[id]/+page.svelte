5abb04595b5119e24d627ccd715076fa927f27db
Julian Hammer <100045739+MoinJulian@users.noreply.github.com>
2023-11-23 21:08:25 +0000

    Add a scss for all Rules and edit Styles of em Elements

--- golfrules/src/routes/die-grundlagen-des-spiels/[id]/+page.svelte
+++ golfrules/src/routes/die-grundlagen-des-spiels/[id]/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Toc from '$lib/components/TOC.svelte';
+	import '$lib/scss/rules.scss';
 
 	export let data;
 
@@ -21,57 +22,3 @@
 <article>
 	{@html html_code}
 </article>
-
-<style lang="scss">
-	article {
-		:global(h2) {
-			padding-top: 5rem;
-			margin-top: -3.5rem;
-		}
-
-		:global(blockquote) {
-			margin-block: 1rem;
-			margin-left: 1rem;
-			padding-left: 0.5rem;
-			color: var(--secondary-font-color);
-			font-size: var(--small-font);
-			border-left: 0.2rem solid var(--border-color);
-			line-height: 1.6;
-		}
-
-		:global(pre) {
-			border-radius: 0.4rem;
-			font-size: var(--small-font);
-			padding: 1rem;
-			margin-block: 1rem;
-			overflow: auto;
-			max-height: 30rem;
-			border: 0.15rem solid var(--code-border-color);
-			background-color: var(--code-bg-color);
-			tab-size: 4;
-
-			scrollbar-width: thin;
-			&::-webkit-scrollbar {
-				width: 0.4rem;
-				height: 0.4rem;
-			}
-			&::-webkit-scrollbar-thumb {
-				background-color: var(--secondary-font-color);
-			}
-		}
-
-		:global(code:not(pre code)) {
-			font-family: monospace;
-			background-color: var(--inline-code-bg-color);
-			padding-inline: 0.5rem;
-			padding-block: 0.05rem;
-			border-radius: 0.2rem;
-			font-size: var(--small-font);
-			white-space: nowrap;
-		}
-
-		:global(strong em) {
-			color: red;
-		}
-	}
-</style>
