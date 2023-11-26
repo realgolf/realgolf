081a73a102ff753d4706c49107a1ebea690eb1d2
Julian Hammer <100045739+MoinJulian@users.noreply.github.com>
2023-11-26 21:29:21 +0000

    Initial IOS App with Swift

---
+++ game/ios/Golf Games/Golf Games/Golf_GamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Golf_GamesApp.swift
+//  Golf Games
+//
+//  Created by Julian on 26/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Golf_GamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
