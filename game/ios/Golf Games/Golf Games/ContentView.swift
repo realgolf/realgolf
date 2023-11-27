abf0c908750d37af49831c846a49b7e3e6d8109a
Julian Hammer <100045739+MoinJulian@users.noreply.github.com>
2023-11-27 19:32:38 +0000

    First implementation of the Dashboard

--- game/ios/Golf Games/Golf Games/ContentView.swift
+++ game/ios/Golf Games/Golf Games/ContentView.swift
@@ -1,25 +1,49 @@
-//
-//  ContentView.swift
-//  Golf Games
-//
-//  Created by Julian on 26/11/2023.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    // Simulating the PageData structure
+    struct PageData {
+        var name: String
+    }
+
+    // Simulating the imported Card component
+    struct Card: View {
+        var label: String
+        var content: String
+
+        var body: some View {
+            VStack {
+                Text(label).font(.title)
+                Text(content).padding()
+            }.border(Color.black, width: 1).padding()
+        }
+    }
+
+    // Simulating the imported types
+    struct SvelteTypes {}
+
+    @State private var data = PageData(name: "John Doe")
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, Golf Games!")
-        }
-        .padding()
+            Text("Dashboard").font(.largeTitle).padding()
+
+            Text("Welcome to \"Golf Games\" \(data.name) - your ultimate destination for golf challenges!\nDiscover \"4 Winning\", \"Exact\", and \"Precision\" - captivating golf experiences tested with trainers.")
+                .font(.body).bold().padding()
+
+            Card(label: "Modi", content: "You can find all the games in the").background(Color.green).foregroundColor(Color.white)
+
+            Card(label: "Rules", content: "Everything about the Rules is in the").background(Color.green).foregroundColor(Color.white)
+
+            Card(label: "Levels", content: "If the normal games are too boring, you can find more challenging ideas in").background(Color.green).foregroundColor(Color.white)
+
+            Text("But most important, enjoy the games and have fun!").font(.title2).padding()
+        }.padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
- 
