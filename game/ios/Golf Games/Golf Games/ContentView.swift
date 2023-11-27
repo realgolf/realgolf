24598b893a641e49f43f6470628c201d53b0d5e2
Julian Hammer <100045739+MoinJulian@users.noreply.github.com>
2023-11-27 20:00:32 +0000

    Updated IOS Code and added first Dashboard view

--- game/ios/Golf Games/Golf Games/ContentView.swift
+++ game/ios/Golf Games/Golf Games/ContentView.swift
@@ -11,34 +11,67 @@ struct ContentView: View {
         var label: String
         var content: String
 
+        @Environment(\.colorScheme) var colorScheme
+
         var body: some View {
             VStack {
                 Text(label).font(.title)
                 Text(content).padding()
-            }.border(Color.black, width: 1).padding()
+            }
+            .border(borderColor, width: 1)
+            .padding()
+        }
+
+        var borderColor: Color {
+            return colorScheme == .dark ? .white : .black
         }
     }
 
     // Simulating the imported types
     struct SvelteTypes {}
 
-    @State private var data = PageData(name: "John Doe")
+    @State private var data = PageData(name: "Julian Hammer")
+
+    @Environment(\.colorScheme) var colorScheme
 
     var body: some View {
-        VStack {
-            Text("Dashboard").font(.largeTitle).padding()
+        NavigationView {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 16) {
+                    Text("Dashboard").font(.largeTitle).padding()
 
-            Text("Welcome to \"Golf Games\" \(data.name) - your ultimate destination for golf challenges!\nDiscover \"4 Winning\", \"Exact\", and \"Precision\" - captivating golf experiences tested with trainers.")
-                .font(.body).bold().padding()
+                    Text("Welcome to \"Golf Games\" \(data.name) - your ultimate destination for golf challenges!\nDiscover \"4 Winning\", \"Exact\", and \"Precision\" - captivating golf experiences tested with trainers.")
+                        .font(.body)
+                        .bold()
+                        .padding()
+                        .lineLimit(nil)
+                        .background(backgroundBasedOnColorScheme())
+                        .border(borderColorBasedOnColorScheme(), width: 1)
 
-            Card(label: "Modi", content: "You can find all the games in the").background(Color.green).foregroundColor(Color.white)
+                    Card(label: "Modi", content: "Here you can find all game modes you can play, just click on the link for the game you want to play.")
+                        .background(backgroundBasedOnColorScheme())
 
-            Card(label: "Rules", content: "Everything about the Rules is in the").background(Color.green).foregroundColor(Color.white)
+                    Card(label: "Rules", content: "Everything about the Rules is in the")
+                        .background(backgroundBasedOnColorScheme())
 
-            Card(label: "Levels", content: "If the normal games are too boring, you can find more challenging ideas in").background(Color.green).foregroundColor(Color.white)
+                    Card(label: "Levels", content: "If the normal games are too boring, you can find more challenging ideas in")
+                        .background(backgroundBasedOnColorScheme())
 
-            Text("But most important, enjoy the games and have fun!").font(.title2).padding()
-        }.padding()
+                    Spacer()
+
+                    Text("But most important, enjoy the games and have fun!").font(.title2).padding()
+                }
+                .padding()
+            }
+        }
+    }
+
+    func backgroundBasedOnColorScheme() -> Color {
+        return colorScheme == .dark ? .black : .white
+    }
+
+    func borderColorBasedOnColorScheme() -> Color {
+        return colorScheme == .dark ? .white : .black
     }
 }
 
