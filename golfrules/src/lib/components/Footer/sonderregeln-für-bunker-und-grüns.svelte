4be0a635fb4998a456a28d9f9ebc53a8ff1efaac
Julian Hammer <100045739+MoinJulian@users.noreply.github.com>
2023-11-30 20:43:45 +0000

    Added Sonderregeln für Bunker und Grüns

---
+++ golfrules/src/lib/components/Footer/sonderregeln-für-bunker-und-grüns.svelte
@@ -0,0 +1,17 @@
+<div class="column">
+	<h3>IV. Sonderregeln für Bunker und Grüns</h3>
+
+	<a href="/sonderregeln-für-bunker-und-grüns/bunker">Regel 12 Bunker</a>
+</div>
+
+<style lang="scss">
+	.column {
+		display: flex;
+		flex-direction: column;
+		padding: 2rem;
+
+		a {
+			font-size: 1rem;
+		}
+	}
+</style>
