537c8bc075a7dcb69ac288834a7100e40d435c96
Julian Hammer <100045739+MoinJulian@users.noreply.github.com>
2023-12-02 18:46:22 +0000

    Initial Regel 14

---
+++ golfrules/src/data/ball-aufnehmen-und-ins-spiel-zurückbringen/vorgehensweisen-mit-dem-ball-markieren-aufnehmen-und-reinigen-an-stelle-zurücklegen-droppen-im-rrleichterungsbereich-spielen-vom-falschen-ort.md
@@ -0,0 +1,3 @@
+---
+title: Regel 14 - Vorgehensweisen mit dem Ball - Markieren, Aufnehmen und Reinigen; an Stelle zurücklegen; Droppen im Erleichterungsbereich; Spielen vom falschen Ort
+---
---
+++ golfrules/src/lib/components/Footer/ball-aufnehmen-und-ins-spiel-zurückbringen.svelte
@@ -0,0 +1,22 @@
+<div class="column">
+	<h3>V. Ball aufnehmen und ins Spiel zurückbringen</h3>
+
+	<a
+		href="/ball-aufnehmen-und-ins-spiel-zurückbringen/vorgehensweisen-mit-dem-ball-markieren-aufnehmen-und-reinigen-an-stelle-zurücklegen-droppen-im-rrleichterungsbereich-spielen-vom-falschen-ort"
+		>Vorgehensweisen mit dem Ball: Markieren, Aufnehmen und Reinigen; an Stelle zurücklegen; Droppen
+		im Erleichterungsbereich; Spielen vom falschen Ort</a
+	>
+</div>
+
+<style lang="scss">
+	.column {
+		display: flex;
+		flex-direction: column;
+		padding: 2rem;
+		width: 30vw;
+
+		a {
+			font-size: 1rem;
+		}
+	}
+</style>
