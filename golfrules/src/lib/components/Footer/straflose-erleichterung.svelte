f4299f6d1bf75a24e49e789764a94b4dbac8e064
Julian Hammer <100045739+MoinJulian@users.noreply.github.com>
2023-12-06 11:11:53 +0000

    Updated link in Straflose Erleichteung

--- golfrules/src/lib/components/Footer/straflose-erleichterung.svelte
+++ golfrules/src/lib/components/Footer/straflose-erleichterung.svelte
@@ -5,7 +5,7 @@
 <div class="column">
 	<h3>VI. Straflose Erleichterung</h3>
 	<a
-		href="/straflose-erleichterung/erleichterung-von-losen-hinderlichen-naturstoffen-und-beweglichen-hemmnissen-einschließlich-ball-oder-ballmarker-die-das-spiel-unterstützen-oder-beeinträchtigen"
+		href="/straflose-erleichterung/erleichterung-von-losen-hinderlichen-naturstoffen-und-beweglichen-hemmnissen-einschließlich-ball-oder-ballmarker-die-das-piel-unterstützen-oder-beeinträchtigen"
 	>
 		Regel 15 - Erleichterung von losen hinderlichen Naturstoffen und beweglichen Hemmnissen
 		(einschließlich Ball oder Ballmarker, die das Spiel unterstützen oder beeinträchtigen)</a
