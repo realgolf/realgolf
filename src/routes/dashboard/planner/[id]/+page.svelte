d3242dbcdc99d88932b79bb8df476bd130b56ad5
Julian Hammer <jh@moinjulian.com>
2024-05-08 20:45:23 +0100

    Add Description and Plan to Planner Page

--- src/routes/dashboard/planner/[id]/+page.svelte
+++ src/routes/dashboard/planner/[id]/+page.svelte
@@ -1,18 +1,38 @@
 <script lang="ts">
-    export let data;
-
+	export let data;
 </script>
+
 <svelte:head>
-    <title>Trainings Plan - {data.id}</title>
+	<title>Trainings Plan - {data.id}</title>
 </svelte:head>
 
 <h1>{data.title} - {data.id}</h1>
 
-<p class="date">{data.dateOfCreation?.toLocaleDateString()} - {data.dateOfLastEdit?.toLocaleDateString()}</p>
+<p class="date">
+	{data.dateOfCreation?.toLocaleDateString()} - {data.dateOfLastEdit?.toLocaleDateString()}
+</p>
+
+<p>Description</p>
+<div class="description">
+	{#if data.description}
+		<p>{data.description}</p>
+	{:else}
+		<p class="error">No description available</p>
+	{/if}
+</div>
+
+<p>Plan</p>
+<div class="plan">
+	{#if data.plan}
+		<p>{data.plan}</p>
+	{:else}
+		<p class="error">No plan available</p>
+	{/if}
+</div>
 
 <style lang="scss">
-    .date {
-        font-size: 0.8em;
-        color: #666;
-    }
+	.date {
+		font-size: 0.8em;
+		color: #666;
+	}
 </style>
