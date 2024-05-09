8ce4511901dbe6852c3138587157db1d1ef566cf
Julian Hammer <jh@moinjulian.com>
2024-05-09 21:11:37 +0100

    Add link and description about planners on public profile page

---
+++ src/lib/components/Public/Planner.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { PageData } from '../../../routes/[name]/$types';
+
+	export let data: PageData;
+</script>
+
+<div class="planner">
+	<p>
+		You can view all of {data.user_username} Planners
+		<a href="/{data.user_username}/planner">here</a>. You can star them to let {data.user_username} know
+		that you like his Planner and thinking aboout using it for your routine as well.
+	</p>
+</div>
