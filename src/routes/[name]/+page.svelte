cb8fad47c50272f4e2f640acb43a42049a913d83
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2024-05-15 09:53:52 +0100

    Add User not Found page

--- src/routes/[name]/+page.svelte
+++ src/routes/[name]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import Info from '$lib/components/Public/Info.svelte';
 	import { onMount } from 'svelte';
+	import { _, isLoading } from 'svelte-i18n';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
@@ -22,4 +23,40 @@
 	<title>{data.user_username} ({data.user_name})</title>
 </svelte:head>
 
-<Info {data} />
+{#if $isLoading}
+	<p>Loading...</p>
+{:else if data.user_username === undefined}
+	<section>
+		<div>
+			<h1>{$_('error')}</h1>
+			<pre>404: {$_('user_not_found')}</pre>
+			<br />
+			<a href="/">{$_('home')}</a>
+		</div>
+	</section>
+{:else}
+	<Info {data} />
+{/if}
+
+<style lang="scss">
+	section {
+		width: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		div {
+			text-align: left;
+			line-height: 1rem !important;
+		}
+	}
+
+	h1 {
+		padding-block: 1rem;
+		color: yellow;
+	}
+
+	pre {
+		color: yellow;
+	}
+</style>
