f1fb384806dd0cf640c351aeae2b8bf834a00b44
MoinJulian <jh@moinjulian.com>
2024-05-20 21:10:17 +0100

    Add Golf Putt Animation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import Footer from '$lib/components/Footer.svelte';
 	import Nav from '$lib/components/Nav/Nav.svelte';
+	import Offline from '$lib/components/Offline.svelte';
 	import ScrollUp from '$lib/components/ScrollUp.svelte';
 	import '$lib/scss/app.scss';
 	import Cookies from 'js-cookie';
@@ -86,11 +87,7 @@
 
 	<Footer />
 {:else}
-	<div class="center">
-		<img src="/logo.PNG" alt="Real Golf Games Logo" />
-		<h1>Offline</h1>
-		<p>Please check your network connection</p>
-	</div>
+	<Offline />
 {/if}
 
 <style lang="scss">
@@ -101,20 +98,6 @@
 
 		@media screen and (max-width: 767px) {
 			margin-bottom: 20px;
-		}
-	}
-
-	.center {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		width: 100vw;
-		height: 100vh;
-		flex-direction: column;
-
-		img {
-			width: 256px;
-			height: 256px;
 		}
 	}
 </style>
