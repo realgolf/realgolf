cf54c30551fca272c0ccd8f55d07e9d84e81b849
MoinJulian <jh@moinjulian.com>
2024-05-21 19:04:13 +0100

    Add test for generateRandomNumber

---
+++ tests/scripts/Precision/generateRandomNumber.spec.ts
@@ -0,0 +1,24 @@
+import { generateRandomNumber } from '../../../src/lib/scripts/Precision/generateRandomNumber';
+
+describe('generateRandomNumber', () => {
+	it('should generate a random number within the specified range', () => {
+		const range = 10;
+		const lowerRange = 5;
+		const upperRange = 15;
+
+		const randomNumber = generateRandomNumber(range, lowerRange, upperRange);
+
+		expect(randomNumber).toBeGreaterThanOrEqual(lowerRange);
+		expect(randomNumber).toBeLessThan(upperRange);
+	});
+
+	it('should return a rounded number', () => {
+		const range = 10;
+		const lowerRange = 5;
+		const upperRange = 15;
+
+		const randomNumber = generateRandomNumber(range, lowerRange, upperRange);
+
+		expect(randomNumber).toBe(Math.ceil(randomNumber));
+	});
+});
