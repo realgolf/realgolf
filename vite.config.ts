b1805e3a7aeb0bf317cb4736d68d34b34f5a9f94
Julian Hammer <jh@moinjulian.com>
2024-05-30 13:02:28 +0200

    feat: display package version to user
    The package version is now displayed to the user in the Dropwdown menu below the Sign Out button.
    
    #837

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { readFileSync } from 'fs';
 import { IncomingMessage, Server, ServerResponse } from 'http';
+import { fileURLToPath } from 'url';
 import type { UserConfig, ViteDevServer } from 'vite';
 import { handle_sockets } from './server/sockets';
+
+const file = fileURLToPath(new URL('package.json', import.meta.url));
+const json = readFileSync(file, 'utf8');
+const pkg = JSON.parse(json);
 
 const socket_io_plugin = {
 	name: 'socket.io plugin',
@@ -11,7 +17,10 @@ const socket_io_plugin = {
 };
 
 const config: UserConfig = {
-	plugins: [sveltekit(), socket_io_plugin]
+	plugins: [sveltekit(), socket_io_plugin],
+	define: {
+		PKG: pkg
+	}
 };
 
 export default config;
