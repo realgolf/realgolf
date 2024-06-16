a47d2f58127c8d86f58df7de34a7861249d061a6
Julian Hammer <jh@moinjulian.com>
2024-06-16 12:18:07 +0100

    chore: show player names and fields for all colors in the archive

--- src/lib/components/Archive/Game.svelte
+++ src/lib/components/Archive/Game.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	// Importing the openGame function from the specified file
+	// Importing necessary functions and components
 	import { openGame } from '$lib/scripts/Archive/openGame';
-	// Importing the Dialog component from the specified file
+	import { onMount } from 'svelte';
 	import { _ } from 'svelte-i18n';
 	import Dialog from '../Global/Dialog.svelte';
 
@@ -13,31 +13,66 @@
 	export let data: string;
 	export let is_over: boolean;
 
-	const dataObj = JSON.parse(data);
 	let interleavedData: { color: string; value: string }[] = [];
+	let players: string[] = [];
 
-	// Check if "4winning" is not included in teams
-	if (teams.includes('4winning')) {
-		// Separate data arrays for each color
-		const redData = dataObj.find((obj: { color: string }) => obj.color === 'red')?.data || [];
-		const blueData = dataObj.find((obj: { color: string }) => obj.color === 'blue')?.data || [];
-		const greenData = dataObj.find((obj: { color: string }) => obj.color === 'green')?.data || [];
-		const orangeData = dataObj.find((obj: { color: string }) => obj.color === 'orange')?.data || [];
-		const pinkData = dataObj.find((obj: { color: string }) => obj.color === 'pink')?.data || [];
-		const yellowData = dataObj.find((obj: { color: string }) => obj.color === 'yellow')?.data || [];
+	function loadData(data: string) {
+		const dataObj = JSON.parse(data);
 
-		// Interleave the data arrays
-		for (let i = 0; i < Math.max(redData.length, blueData.length); i++) {
-			if (i < redData.length) interleavedData.push({ color: 'red', value: redData[i] });
-			if (i < blueData.length) interleavedData.push({ color: 'blue', value: blueData[i] });
-			if (i < greenData.length) interleavedData.push({ color: 'green', value: greenData[i] });
-			if (i < orangeData.length) interleavedData.push({ color: 'orange', value: orangeData[i] });
-			if (i < pinkData.length) interleavedData.push({ color: 'pink', value: pinkData[i] });
-			if (i < yellowData.length) interleavedData.push({ color: 'yellow', value: yellowData[i] });
+		// Check if "4winning" is included in teams
+		if (teams.includes('4winning')) {
+			if (dataObj.map((obj: { name: string }) => obj.name) != undefined) {
+				// Populate players array
+				players = dataObj
+					.map((obj: { name: string }) => obj.name)
+					.filter((name: string) => name != undefined);
+				console.log(players);
+			}
+
+			// Separate data arrays for each color
+			const redData = dataObj.find((obj: { color: string }) => obj.color === 'red')?.data || [];
+			const blueData = dataObj.find((obj: { color: string }) => obj.color === 'blue')?.data || [];
+			const greenData = dataObj.find((obj: { color: string }) => obj.color === 'green')?.data || [];
+			const orangeData =
+				dataObj.find((obj: { color: string }) => obj.color === 'orange')?.data || [];
+			const pinkData = dataObj.find((obj: { color: string }) => obj.color === 'pink')?.data || [];
+			const yellowData =
+				dataObj.find((obj: { color: string }) => obj.color === 'yellow')?.data || [];
+
+			// Interleave the data arrays
+			for (
+				let i = 0;
+				i <
+				Math.max(
+					redData.length,
+					blueData.length,
+					greenData.length,
+					orangeData.length,
+					pinkData.length,
+					yellowData.length
+				);
+				i++
+			) {
+				if (i < redData.length) interleavedData.push({ color: 'red', value: redData[i] });
+				if (i < blueData.length) interleavedData.push({ color: 'blue', value: blueData[i] });
+				if (i < greenData.length) interleavedData.push({ color: 'green', value: greenData[i] });
+				if (i < orangeData.length) interleavedData.push({ color: 'orange', value: orangeData[i] });
+				if (i < pinkData.length) interleavedData.push({ color: 'pink', value: pinkData[i] });
+				if (i < yellowData.length) interleavedData.push({ color: 'yellow', value: yellowData[i] });
+			}
+
+			console.log(interleavedData);
 		}
 
-		console.log(interleavedData);
+		interleavedData = interleavedData;
+		players = players;
+
+		return { interleavedData, players };
 	}
+
+	onMount(() => {
+		loadData(data);
+	});
 </script>
 
 <div class="game">
@@ -54,14 +89,22 @@
 		<button>{$_('update_name')}</button>
 	</form>
 	<p>{$_('created_at', { values: { date: new Date(date).toLocaleDateString() } })}</p>
+	{#if players.length > 0}
+		<h2>{$_('players')}</h2>
+		<ul class="players">
+			{#each players as player}
+				<li>{player}</li>
+			{/each}
+		</ul>
+	{/if}
 	<ul>
 		{#each interleavedData as data}
 			<li class="{data.color} hit">{data.color} - {data.value}</li>
 		{/each}
 	</ul>
-	{#if is_over == true}
+	{#if is_over === true}
 		<p>{$_('game_has_finished', { values: { name } })}</p>
-	{:else if is_over == false}
+	{:else if is_over === false}
 		<p>{$_('game_can_continue', { values: { name } })}</p>
 	{:else}
 		<p>{$_('no_game_state')}</p>
@@ -92,6 +135,14 @@
 		color: orange;
 	}
 
+	.pink {
+		color: pink;
+	}
+
+	.yellow {
+		color: yellow;
+	}
+
 	.hit {
 		margin-left: 1rem;
 	}
@@ -100,6 +151,17 @@
 		margin-left: 1rem;
 		padding-right: 0.5rem;
 		color: var(--font-color);
+	}
+
+	.players {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		list-style: none;
+
+		li {
+			margin-right: 1rem;
+		}
 	}
 
 	@import '$lib/scss/Archive/game.scss';
