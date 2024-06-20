8b26e7be72ce2cdcb1ad45fa820416a5da93e690
Julian Hammer <jh@moinjulian.com>
2024-06-20 09:24:32 +0100

    chore: update courses welcome page

--- src/routes/dashboard/courses/+page.svelte
+++ src/routes/dashboard/courses/+page.svelte
@@ -1,34 +1,35 @@
 <script lang="ts">
+	import sanitizeHTML from '$lib/shared/utils/sanitizeHTML';
 	import { _, isLoading } from 'svelte-i18n';
-	import type { PageData } from './$types';
-	export let data: PageData;
+	export let data: any;
 
 	let user_name = data.name;
 	let handicap = data.handicap;
 </script>
 
 <svelte:head>
-	<title>{$_("score_tracker")}</title>
+	<title>{$_('score_tracker')}</title>
 </svelte:head>
 
 {#if $isLoading}
 	<p>Loading...</p>
 {:else}
-	<h1>{$_("score_tracker")}</h1>
+	<h1>{$_('score_tracker')}</h1>
 
 	<main>
-	<div class="heading">
-		<p>
-			{$_('welcome_to_course_tracker', { values: { user_name, handicap } })}
-		</p>
+		<div class="heading">
+			<p
+				use:sanitizeHTML={[
+					$_('welcome_to_course_tracker', { values: { user_name: user_name, handicap: handicap } })
+				]}
+			></p>
 
-		<a href="/dashboard/courses/play">{$_("play")}</a>
-	</div>
-</main>
+			<!-- <a href="/dashboard/courses/play">{$_('play')}</a> -->
+		</div>
+	</main>
 
 	<main>
-		<h4>{$_('welcome_to_course_tracker', { values: { user_name } })}</h4>
-		<p class="error">{$_("site_under_development")}</p>
-		<a href="/dashboard"><b>{$_("back_to_dashboard")}</b></a>
+		<p class="error">{$_('site_under_development')}</p>
+		<a href="/dashboard"><b>{$_('back_to_dashboard')}</b></a>
 	</main>
 {/if}
