8a80fd9136950f676bd115d316626f3f26d6617f
Julian Hammer <jh@moinjulian.com>
2024-06-20 10:55:37 +0100

    chore: sort course by location from user
    Sorting courses by the distance from the user as well as displaying the distance the user is away
    from the course

--- src/routes/dashboard/courses/play/+page.svelte
+++ src/routes/dashboard/courses/play/+page.svelte
@@ -9,12 +9,40 @@
 	let longitude: number;
 	let courses = data.courses;
 
+	function getDistanceFromLatLonInKm(lat1: number, lon1: number, lat2: number, lon2: number) {
+		const R = 6371; // Radius of the earth in km
+		const dLat = deg2rad(lat2 - lat1);
+		const dLon = deg2rad(lon2 - lon1);
+		const a =
+			Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+			Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+		const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+		const distance = R * c; // Distance in km
+		return distance;
+	}
+
+	function deg2rad(deg: number) {
+		return deg * (Math.PI / 180);
+	}
+
 	onMount(() => {
 		window.navigator.geolocation.getCurrentPosition((position) => {
 			latitude = position.coords.latitude;
 			longitude = position.coords.longitude;
 
-			console.log(latitude, longitude);
+			courses = courses.map((course) => {
+				course.distance = getDistanceFromLatLonInKm(
+					latitude,
+					longitude,
+					course.geolocation.latitude,
+					course.geolocation.longitude
+				);
+				return course;
+			});
+
+			courses.sort((a, b) => (a.distance && b.distance ? a.distance - b.distance : 0));
+
+			console.log(courses); // Courses now sorted by distance
 		});
 	});
 </script>
@@ -35,6 +63,11 @@
 					<p><span>{$_('name')}:</span> {course.name}</p>
 					<p><span>{$_('location')}:</span> {course.location}</p>
 					<p><span>{$_('par')}:</span> {course.total_par}</p>
+					{#if course.distance == undefined}
+						<p><span>{$_('distance')}:</span> Calculating...</p>
+					{:else}
+						<p><span>{$_('distance')}:</span> {course.distance.toFixed(0)} km</p>
+					{/if}
 				</div>
 			{/each}
 		</div>
