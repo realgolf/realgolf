f7c9532eed73ef84f2accdf04c4d006dc4c65d40
Julian Hammer <jh@moinjulian.com>
2024-06-21 13:43:11 +0100

    chore: get games in /games from the game collection

--- src/routes/games/+page.server.ts
+++ src/routes/games/+page.server.ts
@@ -1,27 +1,23 @@
-import { User_Model } from '$lib/server/user/models';
+import { Game_Model } from '$lib/server/games/models';
 
 import type { PageServerLoad } from '../$types';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const load: PageServerLoad = async (event: any) => {
 	try {
-		const all_users = await User_Model?.find();
-		const param_id: string = event.params.id;
+		let games = await Game_Model.find().sort({ date: 1 });
 
-		if (!all_users) {
-			return { status: 404, error: new Error('Users could not be found') };
+		if (!games) {
+			return { status: 404, error: new Error('Games could not be found') };
 		}
 
-		// Collect all games from all users
-		const user_games = all_users.flatMap((user) => user.games);
-
-		const games = user_games.map((game) => {
+		games = games.map((game) => {
 			const gameCopy = JSON.parse(JSON.stringify(game));
 			delete gameCopy._id; // Remove the _id field
 			return gameCopy;
 		});
 
-		return { param_id, games };
+		return { games };
 	} catch (error) {
 		return { status: 500, error: new Error('An unexpected error occurred') };
 	}
