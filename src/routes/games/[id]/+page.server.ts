19ba8c760acc68dc208cf9d1435c470e70576c09
Julian Hammer <jh@moinjulian.com>
2024-07-08 18:29:29 +0200

    chore: add comment function
    Users can add comments to games from other users or them self

--- src/routes/games/[id]/+page.server.ts
+++ src/routes/games/[id]/+page.server.ts
@@ -1,5 +1,7 @@
 import { Game_Model } from '$lib/server/games/models';
+import { User_Model } from '$lib/server/user/models';
 import type { PageServerLoad } from '../$types';
+import { Actions } from './$types';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const load: PageServerLoad = async (event: any) => {
@@ -22,5 +24,52 @@ export const load: PageServerLoad = async (event: any) => {
 		return { param_id, game };
 	} catch (error) {
 		return { status: 500, error: new Error('An unexpected error occurred') };
+	}
+};
+
+export const actions: Actions = {
+	default: async (event) => {
+		const email = event.cookies.get('email');
+		const formData = await event.request.formData();
+		const comment = formData.get('comment') as string;
+		const user = await User_Model.findOne({ 'user.email': email });
+		let game = await Game_Model.findOne({ id: event.params.id });
+		let user_game = user?.games.find((game) => game.id === event.params.id);
+
+		if (!user) {
+			return { status: 401, error: new Error('User not found') };
+		}
+
+		if (!game) {
+			return { status: 404, error: new Error('Game not found') };
+		}
+
+		try {
+			game.comments.push({
+				id: user.id,
+				username: user?.user.username,
+				date: new Date(),
+				content: comment,
+				reactions: [],
+				replies: [],
+				edited: false
+			});
+
+			await game.save();
+
+			user_game?.comments.push({
+				id: user.id,
+				username: user?.user.username,
+				date: new Date(),
+				content: comment,
+				reactions: [],
+				replies: [],
+				edited: false
+			});
+
+			await user.save();
+		} catch (error) {
+			return { status: 500, error: new Error('An unexpected error occurred') };
+		}
 	}
 };
--- src/routes/games/[id]/+page.svelte
+++ src/routes/games/[id]/+page.svelte
@@ -12,20 +12,20 @@
 </script>
 
 <svelte:head>
-	<title>{$_('game')} - {data.game.id}</title>
+	<title>{$_('game')} - {data?.game.id}</title>
 </svelte:head>
 
 {#if $isLoading}
 	<p>Loading...</p>
 {:else}
-	<button on:click={go_back}><Fa icon={faArrowLeft}></Fa></button>
+	<button class="back" on:click={go_back}><Fa icon={faArrowLeft}></Fa></button>
 
 	<h1>
 		{#if data.game}
 			{$_('public_game', {
 				values: {
 					game_name: data.game.name,
-					game_date: new Date(data.game.date).toLocaleDateString()
+					game_date: new Date(data?.game.date).toLocaleDateString()
 				}
 			})}
 		{:else}
@@ -33,12 +33,12 @@
 		{/if}
 	</h1>
 
-	{#if data.game.owner}
+	{#if data?.game.owner}
 		<h2>{$_('owner')}</h2>
 		<p>{data.game.owner}</p>
 	{/if}
 
-	{#if data.game.type}
+	{#if data?.game.type}
 		<h2>{$_('type')}</h2>
 		<p>{data.game.type}</p>
 	{/if}
@@ -46,16 +46,59 @@
 	{#if data.game}
 		<p class="data">{data.game.data}</p>
 
-		{#if data.game.teams.includes('4winning_')}
+		{#if data?.game?.teams.includes('4winning_')}
 			<FourWinningTable measurement_units="yards" data={data.game.data} />
 		{/if}
 	{/if}
+
+	<div class="comments">
+		<h2>{$_('comments')}</h2>
+		{#if data?.game?.comments.length === 0}
+			<p>{$_('no_comments')}</p>
+		{:else}
+			{#each data?.game.comments as comment}
+				<div class="comment">
+					<p>{comment.content}</p>
+					<p>{comment.date}</p>
+				</div>
+			{/each}
+		{/if}
+		<form method="POST" autocomplete="off" autocorrect="on" id="add_new_comment_form">
+			<textarea name="comment" placeholder={$_('comment')}></textarea>
+			<br />
+			<button type="submit">{$_('submit')}</button>
+		</form>
+	</div>
 {/if}
 
-<style>
-	button {
+<style lang="scss">
+	.back {
 		background-color: transparent;
 		border: none !important;
 		outline: none !important;
+	}
+
+	.comments {
+		width: 100%;
+
+		.comment {
+			border: 1px solid var(--border-color);
+			margin: 10px 0;
+			padding: 10px;
+		}
+
+		#add_new_comment_form {
+			border-top: 2px solid var(--border-color);
+			display: flex;
+			flex-direction: column;
+			margin-top: 20px;
+
+			textarea {
+				margin-top: 10px;
+				margin-bottom: 10px;
+				height: 3rem;
+				padding-left: 5px;
+			}
+		}
 	}
 </style>
