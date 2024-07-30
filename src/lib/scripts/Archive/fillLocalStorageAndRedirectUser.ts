9730c2c1de0bee539d8207385a48051d2e1eb321
Julian Hammer <jh@moinjulian.com>
2024-07-30 20:42:13 +0200

    fix: fix open game for 4winning

--- src/lib/scripts/Archive/fillLocalStorageAndRedirectUser.ts
+++ src/lib/scripts/Archive/fillLocalStorageAndRedirectUser.ts
@@ -6,19 +6,17 @@ import { redirect } from './redirect';
  * @param {string} teams - The teams parameter indicating the number of teams.
  * @param {string} gameData - The game data to be stored in the local storage.
  */
+
+function getTeamLength(teams: string): number {
+	const teamLength = teams.split('_')[1];
+	return parseInt(teamLength);
+}
+
 export function fillLocalStorageAndRedirectUser(teams: string, gameData: string) {
 	localStorage.setItem(teams, gameData);
-	if (teams == '4winning_2_teams') {
-		redirect('/dashboard/games/4Winning/Two-Players');
-	} else if (teams == '4winning_3_teams') {
-		redirect('/dashboard/games/4Winning/Three-Players');
-	} else if (teams == '4winning_4_teams') {
-		redirect('/dashboard/games/4Winning/Four-Players');
-	} else if (teams == 'exact_2_teams') {
-		redirect('/dashboard/games/Exact/Two-Players');
-	} else if (teams == 'exact_3_teams') {
-		redirect('/dashboard/games/Exact/Three-Players');
-	} else if (teams == 'exact_4_teams') {
-		redirect('/dashboard/games/Exact/Four-Players');
+	if (teams.includes('4winning')) {
+		redirect(`/dashboard/games/4Winning/play?team_length=${getTeamLength(teams)}`);
+	} else if (teams.includes('exact')) {
+		redirect(`/dashboard/games/Exact/play?team_length=${getTeamLength(teams)}`);
 	}
 }
